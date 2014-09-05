3e96679f5ffc5698c614796a704a84960ec3372b
Wes Kendall <wes.kendall@tryambition.com>
2014-09-05 19:16:35 -0400

    changed ordering of imports

--- ambition_slack/wsgi.py
+++ ambition_slack/wsgi.py
@@ -6,11 +6,12 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
-from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
 import os
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ambition_slack.settings")
 
+
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 application = Cling(get_wsgi_application())
