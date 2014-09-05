bb9f7ebcb8027acfb38861da0fa3758bde9e49e6
Wes Kendall <wes.kendall@tryambition.com>
2014-09-05 18:50:25 -0400

    updated settings for static root

--- ambition_slack/settings.py
+++ ambition_slack/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    #'django.contrib.staticfiles',
+    'django.contrib.staticfiles',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -81,3 +81,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
