6ef727f9751201180ab8325f548fcedc6d680751
Wes Kendall <wes.kendall@tryambition.com>
2014-09-05 20:36:07 -0400

    added logging for posts for heroku

--- ambition_slack/github/views.py
+++ ambition_slack/github/views.py
@@ -2,19 +2,13 @@ import logging
 
 from django.http import HttpResponse
 from django.views.generic.base import View
-from django.views.decorators.csrf import csrf_exempt
 
 
 LOG = logging.getLogger('console_logger')
 
 
 class GithubView(View):
-    def get(self, request, *args, **kwargs):
-        LOG.info('Github')
-        return HttpResponse('Github')
-
-    @csrf_exempt
     def post(self, request, *args, **kwargs):
         LOG.info('Post')
         LOG.info(request.__dict__)
-        return super(GithubView, self).post(request, *args, **kwargs)
+        return HttpResponse()
