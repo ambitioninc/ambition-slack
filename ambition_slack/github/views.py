cddc920d98d644ed5b9e363300ebb8b0acff7439
Wes Kendall <wes.kendall@tryambition.com>
2014-09-05 20:18:21 -0400

    added simple logging for heroku

--- ambition_slack/github/views.py
+++ ambition_slack/github/views.py
@@ -4,13 +4,14 @@ from django.http import HttpResponse
 from django.views.generic.base import View
 
 
-LOG = logging.getLogger(__name__)
+LOG = logging.getLogger('console_logger')
 
 
 class GithubView(View):
     def get(self, request, *args, **kwargs):
-        LOG.error('Github')
+        LOG.info('Github')
         return HttpResponse('Github')
 
     def post(self, request, *args, **kwargs):
-        pass
+        LOG.info(request.__dict__)
+        return super(GithubView, self).post(request, *args, **kwargs)
--- ambition_slack/settings.py
+++ ambition_slack/settings.py
@@ -86,3 +86,39 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+
+# Logging
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': ('%(asctime)s [%(process)d] [%(levelname)s] ' +
+                       'pathname=%(pathname)s lineno=%(lineno)s ' +
+                       'funcname=%(funcName)s %(message)s'),
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        }
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'console_logger': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        }
+    }
+}
