bd9c08715af606ce208a43faace71249e708f254
Wes Kendall <wes.kendall@tryambition.com>
2014-09-05 22:41:47 -0400

    added more log debugging

--- ambition_slack/github/views.py
+++ ambition_slack/github/views.py
@@ -20,13 +20,13 @@ LOG = logging.getLogger('console_logger')
 class GithubView(View):
     def get(self, *args, **kwargs):
         slack.api_token = os.environ['SLACK_API_TOKEN']
-        LOG.info('slack token', os.environ['SLACK_API_TOKEN'])
-        LOG.info('slack users', slack.users.list())
+        #LOG.info('slack token', os.environ['SLACK_API_TOKEN'])
+        #LOG.info('slack users', slack.users.list())
 
-        gh = Github(os.environ['GITHUB_USER'], os.environ['GITHUB_PASS'])
-        LOG.info('github user', gh.get_user())
+        #gh = Github(os.environ['GITHUB_USER'], os.environ['GITHUB_PASS'])
+        #LOG.info('github user', gh.get_user())
 
-        return HttpResponse('Done')
+        return HttpResponse('Done token {0} {1}'.format(os.environ['SLACK_API_TOKEN']), slack.users.list())
 
     def handle_pull_request(self, payload):
         """
