de17d5dcd7bb64e6175f7db8f52cb8fccf0e3996
Jodything <Jodything@gmail.com>
2014-10-02 15:12:41 -0400

    updated weather test to not call forecast api

--- ambition_slack/weather/tests.py
+++ ambition_slack/weather/tests.py
@@ -1,11 +1,21 @@
 from django.test import TestCase
 from django.test.client import Client
+from mock import patch
 
 
 class TestWeatherView(TestCase):
     def setUp(self):
         self.client = Client()
 
-    def test_weather_view(self):
+    @patch('ambition_slack.weather.weather_summary.forecastio')
+    def test_weather_view(self, forecastio):
+        # Setup the scenario
+        summary = 'It is raining'
+        forecastio.load_forecast.return_value.hourly.return_value.summary = summary
+
+        # Run the code
         response = self.client.get('/weather/')
+
+        # Verify expectation
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(summary, response.content)
