559486d679f26cf4822701caee0fdf03b273afc9
Brett Wise <brettdwise@gmail.com>
2014-10-02 11:00:28 -0400

    things are working in the test environment. slash command works as expected.

--- ambition_slack/weather/views.py
+++ ambition_slack/weather/views.py
@@ -1,29 +1,17 @@
-import forecastio
 import logging
-import slack
-import os
-# import json
+
 from django.http import HttpResponse
 from django.views.generic.base import View
 
-api_key = os.environ['FORECASTIO_API_KEY']
-
-slack.weather_api_token = os.environ['SLACK_WEATHER_API_TOKEN']
+from ambition_slack.weather.weather_summary import weather_summary
 LOG = logging.getLogger('console_logger')
+
+latitude = 35.046772
+longitude = -85.308863
 
 
 class WeatherView(View):
 
-    def get(self, *args, **kwargs):
-        return HttpResponse('weather')
-
-    def post(self, request, *args, **kwargs):
-    	lat = 35.046772
-		lng = -85.308863
-
-		forecast = forecastio.load_forecast(api_key, lat, lng)
-		byHour = forecast.hourly()
-		byHour.summary
-
-        # payload = json.loads(request.body)
-        return HttpResponse()
+    def get(self, request, *args, **kwargs):
+        weather = weather_summary(latitude, longitude)
+        return HttpResponse(weather)
