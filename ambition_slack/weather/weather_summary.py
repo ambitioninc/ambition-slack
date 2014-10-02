e755f2b725370a263c0d08da037ffc96bc49fd1d
Brett Wise <brettwise@users.noreply.github.com>
2014-10-02 15:49:05 -0400

    Update weather_summary.py

--- ambition_slack/weather/weather_summary.py
+++ ambition_slack/weather/weather_summary.py
@@ -4,7 +4,7 @@ import os
 
 def weather_summary(latitude, longitude):
     api_key = os.environ['FORECASTIO_API_KEY']
-    forecast = forecastio.load_forecast(api_key, LATITUDE, LONGITUDE)
+    forecast = forecastio.load_forecast(api_key, latitude, longitude)
     byHour = forecast.hourly()
     message = byHour.summary
     return message
