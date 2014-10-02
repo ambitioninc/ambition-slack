5920ebb43f9d543621c666dd3dba108140e159c7
Jodything <Jodything@gmail.com>
2014-10-02 10:40:15 -0400

    weather slash command added. seperate weather/forecastio file for future weather updates. tests forthcoming

--- ambition_slack/settings.py
+++ ambition_slack/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_nose',
+    'django_extensions',
     'south',
 )
 
---
+++ ambition_slack/weather/weather_summary.py
@@ -0,0 +1,10 @@
+import forecastio
+import os
+
+
+def weather_summary(latitude, longitude):
+    api_key = os.environ['FORECASTIO_API_KEY']
+    forecast = forecastio.load_forecast(api_key, latitude, longitude)
+    byHour = forecast.hourly()
+    message = byHour.summary
+    return message
