cf574e3360ba36afd814798e339ebbe4fd853346
Brett Wise <brettdwise@gmail.com>
2014-10-09 13:41:56 -0400

    changed tabs to spaces in weather_summary so flake8 would be happy.

--- ambition_slack/weather/weather_summary.py
+++ ambition_slack/weather/weather_summary.py
@@ -3,10 +3,10 @@ import os
 
 
 def weather_summary(lat_long):
-	latitude = lat_long[0]
-	longitude = lat_long[1]
-	api_key = os.environ['FORECASTIO_API_KEY']
-	forecast = forecastio.load_forecast(api_key, latitude, longitude)
-	byHour = forecast.hourly()
-	message = byHour.summary
-	return message
+    latitude = lat_long[0]
+    longitude = lat_long[1]
+    api_key = os.environ['FORECASTIO_API_KEY']
+    forecast = forecastio.load_forecast(api_key, latitude, longitude)
+    byHour = forecast.hourly()
+    message = byHour.summary
+    return message
