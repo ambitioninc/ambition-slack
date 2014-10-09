a8d8cf7def4c328e5ebeeff58064d6d045c9b88e
Jodything <Jodything@gmail.com>
2014-10-09 15:35:25 -0400

    random messages for jeff

--- ambition_slack/weather/views.py
+++ ambition_slack/weather/views.py
@@ -1,3 +1,4 @@
+import random
 from django.http import HttpResponse
 from django.views.generic.base import View
 from ambition_slack.weather.lat_long_lookup import lat_long_lookup
@@ -8,7 +9,19 @@ class WeatherView(View):
 
     def get(self, request, *args, **kwargs):
         location = request.GET['text']
-        if location == '':
+        user = request.GET['user_name']
+        answers = [
+            'The answer lies in your heart',
+            'I do not know',
+            'You spelt weather wrong',
+            'Nope',
+            'Shh, I am trying to sleep',
+            'Why do you need to ask?',
+            'Go away. I do not wish to answer at this time.',
+            'The window is 5 feet away. Stop being lazy', ]
+        if user == 'jeff.mcriffey':
+            weather = random.choice(answers)
+        elif location == '':
             lat_long = lat_long_lookup('37403')
             weather = weather_summary(lat_long)
         else:
