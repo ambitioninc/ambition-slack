171f090e57668db02e713e747d0f709c10be201c
Josh Marlow <joshmarlow@gmail.com>
2015-07-23 16:32:25 -0400

    Removed testing junk from celery.py

--- ambition_slack/celery.py
+++ ambition_slack/celery.py
@@ -5,10 +5,7 @@ from __future__ import absolute_import
 import os
 
 from celery import Celery
-from celery.decorators import periodic_task
-from celery.task.schedules import crontab
 from django.conf import settings
-from django.core.mail import EmailMultiAlternatives
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ambition_slack.settings')
 
@@ -17,8 +14,3 @@ app = Celery('ambition_slack')
 # This allows us to configure directly from Django settings - see [0]
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
-
-
-@periodic_task(run_every=(crontab(hour="20", minute="0,15,20,30", day_of_week="1,2,3,4,5")), ignore_result=True)
-def this_is_a_test():
-    EmailMultiAlternatives('Celery test', 'This is a test', '', ['joshmarlow@gmail.com']).send()
