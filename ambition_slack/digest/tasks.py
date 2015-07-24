3d8d8ddd95ad6c584712cfe0f863a5ef76c30540
Josh Marlow <joshmarlow@gmail.com>
2015-07-24 12:20:31 -0400

    Went back to normal scheduling

--- ambition_slack/digest/tasks.py
+++ ambition_slack/digest/tasks.py
@@ -5,9 +5,6 @@ from ambition_slack.digest.morning_digest import send_digest_to_all_slack_users
 
 
 # Execute this task at 14:00 UTC (9:00 or 10:00 EST every day of the week)
-# @periodic_task(run_every=(crontab(hour="14", minute="0", day_of_week="1,2,3,4,5")), ignore_result=True)
-@periodic_task(
-    run_every=(crontab(hour="*", minute="0,10,15,20,25,30,35,40,45,50,55", day_of_week="1,2,3,4,5")),
-    ignore_result=True)
+@periodic_task(run_every=(crontab(hour="14", minute="0", day_of_week="1,2,3,4,5")), ignore_result=True)
 def generate_morning_digests():
     send_digest_to_all_slack_users()
