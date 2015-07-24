d015a8df8d0fca1bb3728754f10917b204ffed73
Josh Marlow <joshmarlow@gmail.com>
2015-07-24 11:26:49 -0400

    Changed task run time for testing

--- ambition_slack/digest/tasks.py
+++ ambition_slack/digest/tasks.py
@@ -5,6 +5,6 @@ from ambition_slack.digest.morning_digest import send_digest_to_all_slack_users
 
 
 # Execute this task at 14:00 UTC (9:00 or 10:00 EST every day of the week)
-@periodic_task(run_every=(crontab(hour="14,15", minute="0,20", day_of_week="1,2,3,4,5")), ignore_result=True)
+@periodic_task(run_every=(crontab(hour="*", minute="0,30", day_of_week="1,2,3,4,5")), ignore_result=True)
 def generate_morning_digests():
     send_digest_to_all_slack_users()
