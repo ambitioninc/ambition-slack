ac4dc49562089418cce6b1d68b1d31ba18c463a6
Josh Marlow <joshmarlow@gmail.com>
2015-08-03 14:51:07 -0400

    Added a timezone field to the SlackUser model

--- ambition_slack/slack/models.py
+++ ambition_slack/slack/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from timezone_field import TimeZoneField
 
 
 class SlackUser(models.Model):
     email = models.TextField(unique=True)
     username = models.TextField(unique=True)
     name = models.TextField()
+    time_zone = TimeZoneField(default='US/Eastern')
 
     def __unicode__(self):
         return self.email
