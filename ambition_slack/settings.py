a19bba783b5cd1a4e9f2921b536772822e9f215d
Josh Marlow <joshmarlow@gmail.com>
2015-08-03 15:16:42 -0400

    Logging should now go to stdout

--- ambition_slack/settings.py
+++ ambition_slack/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 import dj_database_url
 import os
+import sys
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -135,11 +136,12 @@ LOGGING = {
         'console': {
             'level': 'DEBUG',
             'class': 'logging.StreamHandler',
-            'formatter': 'verbose'
+            'formatter': 'verbose',
+            'strm': sys.stdout,
         }
     },
     'loggers': {
-        'console_logger': {
+        '': {
             'handlers': ['console'],
             'level': 'INFO',
         }
