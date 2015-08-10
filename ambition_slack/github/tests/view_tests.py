a4f3e6c61f2b9ead29db1ccd6413c7382cb3b944
Josh Marlow <joshmarlow@gmail.com>
2015-08-10 13:56:25 -0400

    Split up the github tests into separate test files

--- ambition_slack/github/tests/view_tests.py
+++ ambition_slack/github/tests/view_tests.py
@@ -1,130 +1,14 @@
 import json
 
-from django.db import IntegrityError
-from django.test import TestCase, TransactionTestCase
+from django.test import TestCase
 from django.test.client import Client
 from django_dynamic_fixture import G, F
 from mock import patch
 
 from ambition_slack.github.models import GithubUser
-from ambition_slack.slack.models import SlackUser
 from ambition_slack.github.payload import GithubPayload
 from ambition_slack.github.views import GithubView
 
-
-class GithubViewTest(TestCase):
-    def dummy_test(self):
-        self.assertTrue(True)
-
-
-# __________Model tests_______________
-
-class ModelsTest(TransactionTestCase):
-
-    """
-    Tests various aspects of the github models.
-    """
-    def test_multiple_github_per_slack_user_not_allowed(self):
-        # Try to create multiple github users for the same slack user
-        slack_user = SlackUser.objects.create(
-            email='test_email', username='test_username', name='Jody')
-        GithubUser.objects.create(slack_user=slack_user, username='jodything')
-        with self.assertRaises(IntegrityError):
-            GithubUser.objects.create(slack_user=slack_user, username='another_username')
-
-    def test_github_unicode(self):
-        github_user = GithubUser(username='test_username')
-        self.assertEquals(github_user.__unicode__(), 'test_username')
-
-    def test_slack_unicode(self):
-        slack_user = SlackUser(email='test_email')
-        self.assertEquals(slack_user.__unicode__(), 'test_email')
-
-
-class GithubPayloadTests(TestCase):
-    def test_get_assignee(self):
-        payload = {
-            'pull_request': {
-                'assignee': {
-                    'login': 'jodything',
-                },
-            },
-        }
-        self.assertEqual('jodything', GithubPayload(payload).assignee_login)
-
-    def test_get_assignee_returns_none(self):
-        self.assertIsNone(GithubPayload({}).assignee_login)
-
-    def test_get_sender_login(self):
-        payload = {
-            'sender': {
-                'login': 'jodything',
-            },
-        }
-        self.assertEqual('jodything', GithubPayload(payload).sender_login)
-
-    def test_get_sender_login_returns_none(self):
-        self.assertIsNone(GithubPayload({}).sender_login)
-
-    def test_mentions_user_when_pull_request_mentions_user(self):
-        """
-        Verify that we detect when a user is mentioned in the pull request body.
-        """
-        payload = {
-            'pull_request': {
-                'body': '@jaredlewis please review',
-            },
-        }
-        self.assertTrue(GithubPayload(payload).mentions_user('jaredlewis'))
-
-    def test_mentions_user_when_comment_mentions_user(self):
-        """
-        Verify that we detect when a user is mentioned in a comment.
-        """
-        payload = {
-            'comment': {
-                'body': '@jaredlewis please review',
-            },
-        }
-        self.assertTrue(GithubPayload(payload).mentions_user('jaredlewis'))
-
-    def test_mentions_user_returns_false(self):
-        """
-        Verify that we detect when a user is not mentioned comment
-        """
-        payload = {
-            'comment': {
-                'body': '@jaredlewis please review',
-            },
-        }
-        self.assertFalse(GithubPayload(payload).mentions_user('haha-i-*never*-exist'))
-
-    def test_pull_request_html_url(self):
-        payload = {
-            'pull_request': {
-                'html_url': 'http://if-only-i-existed.com',
-            },
-        }
-        self.assertTrue(payload['pull_request']['html_url'], GithubPayload(payload).pull_request_html_url)
-
-    def test_pull_request_html_url_returns_none(self):
-        self.assertIsNone(GithubPayload({}).pull_request_html_url)
-
-    def test_pull_request_comment(self):
-        payload = {
-            'issue': {
-                'pull_request': {
-                    'html_url': 'http://if-only-i-existed.com',
-                },
-            },
-        }
-        self.assertTrue(payload['issue']['pull_request']['html_url'], GithubPayload(payload).pull_request_comment)
-
-    def test_pull_request_comment_returns_none(self):
-        self.assertIsNone(GithubPayload({}).pull_request_comment)
-
-
-# ___________View Tests_______________
 
 class TestGithubViews(TestCase):
     def setUp(self):
